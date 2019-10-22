dbd195f9427c2492506407ddcb960dbd91c849a0
Christian Mena <ricardo.91.gm@gmail.com>
2019-10-22 09:20:32 -0500

    Initial commit

--- src/index.js
+++ src/index.js
@@ -4,15 +4,14 @@ const Routes = require('./routes/index');
 
 const path = require('path');
 
+app.set('port',process.env.PORT || 3000);
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use(Routes);
 
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.set('port',process.env.PORT || 3000);
-
 
 app.listen(3000, () => {
     console.log('Server on port 3000');
